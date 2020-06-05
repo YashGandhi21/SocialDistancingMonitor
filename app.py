5b4a08e9db3ddda710a32d4397534c7a663d5a4f
Aaditya Deshpande <aadityadeshpande1998@gmail.com>
2020-06-05 16:30:42 +0530

    added Flask files and requirements.txt of all modules

---
+++ app.py
@@ -0,0 +1,486 @@
+# author: Aaditya Deshpande
+
+'''
+**************************************************
+General Algorithm:
+
+Step 1] start a camera and set its video_capture object global
+
+Step 2] read frames from video_capture, encode into img and convert tobytes()
+
+Step 3] while not released video_capture yield the frame
+
+Algorithm Ends.
+
+**************************************************
+
+Issues to resolve, 
+
+1) in cameraApi: path for linux environment.
+                if platform.system()=='Windows':
+                    dest = \\.join([target,myfile])
+                elif platform.system()=='Linux':
+                    dest = /.join([target,myfile])
+
+(Requires internet)
+2) Make bootstrap.css locally available in static folder (performance improvement)
+
+3) Reduce global variables by server-side session management
+
+4) Upload video option to make seperate dashboard available.
+
+5) (optional): ip camera from the internet using AngleCam Api 
+
+6) (optional): base templates for jinja2 for extending template logic
+
+
+'''
+
+#Import
+
+from flask import Flask, render_template, Response,jsonify,make_response,request,redirect, url_for
+from cameraApi.camera import webCamera_Stream,stopWebCamera_Stream,startWebCamera_Stream,openVideoFile_Stream
+from cameraApi.camera import facedetection,playVideo,facedetecFromFrame,GetOnlyCamFrame,startipWebCam
+import time
+import cv2
+import requests
+import urllib
+
+#Configuring flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+#global variables
+
+#This data is fetched by website (by JavaScript)
+jsonData = {}
+
+#this is used to share a captured frame with multiple functions
+globalFrame = None
+
+#this is used to control any kind of video streames
+global video_capture
+
+#Workaround used to stop double cam feed...;)
+StopAllFrames = False
+
+#for handling HeatmapFrame
+heatMapFrame = None
+
+#for accessing same ip address in different functions
+ip = None
+
+#First function that gets invoked when visited home page.
+@app.route('/')
+def index():
+    print("User in home page")
+    return render_template('index.html')
+
+
+#stop the stream, No matter who started.
+@app.route('/stopCam',methods=['GET', 'POST'])
+def stopCam():
+    print("inside stop cam")
+    #Resetting the double frame (Workaround)
+    global globalFrame
+    globalFrame = None
+
+    #used to break the double frame loop
+    global StopAllFrames
+    StopAllFrames = True
+
+    #stopping the actual video stream object 
+    global video_capture
+    try:
+        stopWebCamera_Stream(video_capture)
+    except NameError:
+        print("caught nameError of video_Capture only for webcam, direclty hit to /stopCam")
+        pass
+
+    return render_template('stopCam.html')
+
+
+#for local camera display (Camera view only option of index page)
+@app.route('/camera')
+def camera():
+    return render_template('camera.html')
+
+#generate frames and yield as it comes
+def generate_camframe():
+    global video_capture
+    print("inside call start cam")
+    video_capture = startWebCamera_Stream()
+    while True:
+        frame = webCamera_Stream(video_capture) #Comment this in case of different processing
+
+        #frame = GetOnlyCamFrame(video_capture) # HERE WE CAN PROCESS FRAME
+
+        if frame is None:
+            print("found frame is none")
+            break
+        
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+#this method gets invoked by <img src="url_for..."> 
+@app.route('/cam_feed')
+def cam_feed():
+    print("inside cam_feed")
+    #here response will be send frame by frame
+    return Response(generate_camframe(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+#For FACE DETECTION 
+
+
+#on hitting this url, cam.html page will rander
+@app.route('/cam')
+def cam():
+    return render_template('cam.html')
+
+#generate frames and yield as it comes
+def generate_frame():
+    global video_capture
+    print("inside call start cam")
+    video_capture = startWebCamera_Stream()
+    while True:
+        frame , count = facedetection(video_capture) #might give warning when frame is none
+        
+        # HERE WE CAN PROCESS FRAME
+
+        if frame is None:
+            print("found frame is none")
+            break
+        
+        #updating the JSON data so that client can fetch it
+        global jsonData
+        jsonData.update({"count":count})
+        
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+#for sending back response as image (it will be called by <img src="")
+@app.route('/video_feed')
+def video_feed():
+    print("inside video_feed")
+    #here response will be send frame by frame (called by html page)
+    return Response(generate_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+
+#this will act like a REST-API, no matter state of server, client can fetch the data.
+@app.route('/jsonData',methods=['GET', 'POST'])
+def stream_json():
+    global jsonData
+    #print(jsonify(jsonData))
+    return jsonify(jsonData)
+
+
+
+
+#FOR playing DEMO DATA DISPLAY
+
+
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
+
+def generatedemo_frame():
+    global video_capture
+    print("inside call start demo")
+    #video_capture = startWebCamera_Stream()
+    #changed
+
+    video_capture = openVideoFile_Stream()
+    vidStartTime = time.time()
+
+    print("ORIGINAL video Fps is ", video_capture.get(cv2.CAP_PROP_FPS))
+    #print("Changes made status = ",video_capture.set(cv2.CAP_PROP_FPS,23))
+    print("Total number of frames present is ",(video_capture.get(cv2.CAP_PROP_FRAME_COUNT)))
+    origFPS = int(video_capture.get(cv2.CAP_PROP_FPS))
+    frmcnt = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    wid = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    hei = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    vidLen = frmcnt/origFPS
+    print("video length is total {} sec ".format(vidLen))
+    print("video resolution is {} X {} ".format(wid,hei))
+    lpFlag = False
+
+    #Dummy logic to set FPS
+    if wid >= 1280 or hei >= 720:
+        origFPS = origFPS + 11
+    else:
+        origFPS = origFPS + 1
+
+    #origFPS = 70  #set this for manual fps
+
+    while not frmcnt <= 0:
+        
+        t_end = time.time() + 1
+        for i in range(origFPS):
+
+            time.sleep(1/origFPS)
+            frame = playVideo(video_capture,wid,hei)
+            # HERE WE CAN PROCESS FRAME
+            frmcnt = frmcnt -1
+
+            if frame is None:
+                print("found frame is none")
+                lpFlag = True
+                vidBreakTime = time.time()
+                print("total video time ", vidBreakTime - vidStartTime )
+                break
+
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        if lpFlag == True:
+            #print("found frame is none")   
+            print("broken upper loop")
+            break
+
+        if time.time() < t_end:
+            print("inside time adjust")
+            time.sleep(t_end - time.time())
+
+
+
+    # Use this logic for maximum pumping of data
+
+    # while True:
+    #     #frame = webCamera_Stream(video_capture)
+
+    #     #frame , count = facedetection(video_capture) #might give warning when frame is none
+    #     frame = playVideo(video_capture,wid,hei) # HERE WE CAN PROCESS FRAME
+
+
+    #     if frame is None:
+    #         print("found frame is none")
+    #         break
+        
+    #     #global jsonData
+    #     #jsonData.update({"count":count})
+        
+    #     yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/demo_feed')
+def demo_feed():
+    print("inside demo_feed")
+    #here response will be send frame by frame (called by html page)
+    return Response(generatedemo_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+#FOR DOUBLE STREAMING
+
+
+
+@app.route('/double')
+def double():
+    global StopAllFrames
+    StopAllFrames = False
+    return render_template('double.html')
+
+def generatefirst_camframe():
+    global video_capture
+    print("inside call start cam")
+    video_capture = startWebCamera_Stream()
+    while True:
+        #frame = webCamera_Stream(video_capture)
+
+        frame = GetOnlyCamFrame(video_capture) # HERE WE CAN PROCESS FRAME
+
+        if frame is None:
+            print("found frame is none")
+            break        
+        
+        global globalFrame
+        globalFrame = frame
+
+        frame = cv2.imencode('.jpg', frame)[1].tobytes()
+        
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+def generateSecond_camframe():
+    print("i am inside Double frame")
+    global globalFrame
+    global StopAllFrames
+    #count = 0
+    #time.sleep(100)
+    #better use a loop than time, coz it depends upon time to load
+    while True:
+        if globalFrame is None and StopAllFrames == False:
+            #count = count + 1
+            #print("NO second frame count= ",count)
+            continue
+        
+        if StopAllFrames == True:
+            print("Double Frame Loop Broken")
+            break
+
+        FaceFrame = None
+
+        FaceFrame , count = facedetecFromFrame(globalFrame)
+
+        global jsonData
+        jsonData.update({"count":count})
+
+        global heatMapFrame
+
+        heatMapFrame = FaceFrame
+
+        #FaceFrame = cv2.imencode('.jpg', globalFrame)[1].tobytes()
+
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + FaceFrame + b'\r\n')
+
+
+'''
+@app.route('/first') and @app.route('/second') called one after another by html page...(depends)
+
+first and second these driver functions call their respective generate frames
+
+'''
+
+
+@app.route('/first')
+def first():
+    print("inside first feed")
+    #here response will be send frame by frame (called by html page)
+    return Response(generatefirst_camframe(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/second')
+def second():
+    print("received call for double frame")
+    #global globalFaceFrame
+    return Response(generateSecond_camframe(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+# adding feature pause video
+# idea is to, on demand send that perticular single frame
+# on demand means called by JS
+# perticular frame is global face Frame
+
+@app.route('/pauseFirst')
+def pauseFirst():
+    print("paused First frame sent ")
+    pausedFrame = None
+    global globalFrame
+    pausedFrame = cv2.imencode('.jpg', globalFrame)[1].tobytes()
+
+    res = make_response(pausedFrame)
+    res.mimetype= "image/jpg; boundary=frame"
+
+    #No cache to store other wise image will not change.
+    res.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    res.headers['Pragma'] = 'no-cache'
+
+    return res
+
+    #return Response(pausedFrame,mimetype="image/jpg; boundary=frame",)
+
+#ISSUE if global frame is None handle it and cv2.errors as e
+# Issue will be when encoding that image simply redirect to double page
+
+@app.route('/pauseSecond')
+def pauseSecond():
+    print("paused Heatmap frame")
+    global heatMapFrame
+    res1 = make_response(heatMapFrame)
+    res1.mimetype= "image/jpg; boundary=frame"
+
+    #No cache to store other wise image will not change.
+    res1.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    res1.headers['Pragma'] = 'no-cache'
+
+    return res1
+
+
+# FOR IP WEB CAMERA ACCESS
+
+
+# take input ip from the web browser and fetch shots from that ip
+# 
+@app.route('/getip',methods=['GET', 'POST'])
+def getip():
+    global ip
+    ip = request.form.get("ip")
+    print("Found ip is ",ip)
+    print("type is " , type(ip))
+
+    if ip is None:
+        print("inside nonde")
+        #print("sending back to ipcam")
+        return render_template("getip.html",message=" ")
+        #return redirect(url_for("ipcam"))
+    
+    ip = str(request.form.get("ip"))
+
+    try:
+        if urllib.request.urlopen("http://"+ip+"/shot.jpg").getcode() == 200:
+            return redirect(url_for("ipcam"))
+    except urllib.error.URLError as e:
+        print("Wrong URl entered")
+        return render_template("getip.html",message="Wrong Url Entered")
+    
+
+
+    return redirect(url_for("ipcam"))
+    #return render_template("getip.html")
+
+
+
+@app.route('/ipcam',methods=['GET', 'POST'])
+def ipcam():
+    global ip
+    global StopAllFrames
+    StopAllFrames = False
+    #print("req form get ip")
+    #ip = str(request.form.get("ip"))
+    
+    #if ip is None:
+    #    while ip is None:
+    #        continue
+    #print("sending to ipcam template ip is ",ip)
+    return render_template('ipcam.html')
+
+def getIpFrame():
+    global ip
+    #ip = str(request.form.get("ip"))
+
+    if ip is None:
+        print("IP found is none in frame")
+        while ip is None:
+            continue
+
+    print("IP Found inside getipFrame  is = ",ip)
+    while True:
+        #frame = webCamera_Stream(video_capture)
+
+        frame = startipWebCam(ip)
+
+        if frame is None:
+            print("found frame is none")
+            #Stop the second frame
+            break        
+        
+        global globalFrame
+        globalFrame = frame
+        #so that automatically second frame we can stream
+
+        frame = cv2.imencode('.jpg', frame)[1].tobytes()
+        
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/ipstream')
+def ipstream():
+    #print("inside first feed")
+    #here response will be send frame by frame (called by html page)
+    return Response(getIpFrame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=8000, threaded=True)
---
+++ cameraApi/camera.py
@@ -0,0 +1,143 @@
+import cv2
+import requests
+import numpy as np
+# cascPath = 'static\\haarcascade_frontalface_dataset.xml'
+# faceCascade = cv2.CascadeClassifier(cascPath)
+
+#global video_capture
+
+def startWebCamera_Stream():
+    print("Starting web camera")
+    video_capture = cv2.VideoCapture(0)
+    return video_capture
+
+def openVideoFile_Stream():
+    print("open file stream")
+    video_capture = cv2.VideoCapture('videos\\video6.mp4')
+    #video_capture.set(cv2.CAP_PROP_FPS, 10)
+    #print("Changes applied are res = ",video_capture.set(cv2.CAP_PROP_FPS, 30))
+    return video_capture
+
+def webCamera_Stream(video_capture):
+    ret, frame = video_capture.read()
+    if not ret :
+        #video_capture = cv2.VideoCapture(0)   
+        print("Camera can not be opened, some other process using it")
+        return None 
+    frame = cv2.resize(frame, (480,320))
+    return cv2.imencode('.jpg', frame)[1].tobytes()
+
+def stopWebCamera_Stream(video_capture):
+    try:
+        video_capture.release()
+    except NameError:
+        print("caught nameError, direclty hit to /stopCam")
+        pass
+    print("webCamera Stopped")
+
+
+def playVideo(video_capture,wid,hei):
+    ret, frame = video_capture.read()
+    if not ret :
+        print("videocapture reaturened ",ret)   
+        print("Camera can not be opened")
+        return None
+    
+    #frame = cv2.resize(frame,(1280,720))
+    #frame = cv2.resize(frame, (720,480))
+    #frame = cv2.resize(frame, (wid,hei))
+    #frame = cv2.resize(frame, (600,480))
+    frame = cv2.resize(frame, (480,320))
+
+    #frame , _ = facedetecFromFrame(frame)
+
+    frame = cv2.imencode('.jpg', frame)[1].tobytes()
+
+    return frame
+    
+
+
+def facedetection(video_capture):
+    cascPath = 'static\\haarcascade_frontalface_dataset.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    ret, frame = video_capture.read()
+    
+    if not ret :
+        print("videocapture reaturened ",ret)   
+        print("Camera can not be opened")
+        return (None , 0)
+
+    frame = cv2.resize(frame, (480,320))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    #count number of faces found in frame
+    count = len(faces)
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+    frame = cv2.imencode('.jpg', frame)[1].tobytes()
+    
+    return (frame , count)
+
+
+def facedetecFromFrame(frame):
+    cascPath = 'static\\haarcascade_frontalface_dataset.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    frame = cv2.resize(frame, (480,320))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    count = len(faces)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    frame = cv2.imencode('.jpg', frame)[1].tobytes()
+    return (frame, count)
+
+
+def GetOnlyCamFrame(video_capture):
+    ret, frame = video_capture.read()
+    if not ret :   
+        print("Camera can not be opened, some other process using it")
+        return None
+
+    frame = cv2.resize(frame, (480,320))
+
+    return frame
+
+
+def startipWebCam(ip):
+    if ip is not None:
+        camIP = "http://"+ip+"/shot.jpg"
+        img_res = requests.get(camIP)
+        #print("found = ",img_res)
+        img_arr = np.array(bytearray(img_res.content), dtype = np.uint8)
+        try:
+            frame = cv2.imdecode(img_arr,-1)
+            frame = cv2.resize(frame, (480,320))
+            return frame
+        except cv2.error as e:
+            print("IP web Cam is Stopped")
+            return None
+
+        
+    else:
+        return None
+
+
